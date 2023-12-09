ce5b84e229abf44d8c878ba7ae5efecf6a13a1bb
L13k <Kewenlin12@gmail.com>
2023-12-08 21:30:55 -0500

    pf

--- script/Pathfinding/Pathfinding.cs
+++ script/Pathfinding/Pathfinding.cs
@@ -1,59 +1,78 @@
 using Godot;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 
 public partial class Pathfinding : Node{
     Dictionary<Vector2I,Tile_PF> grid;
-    void FindPath(Vector2I startPos, Vector2I targetPos){
 
+    Vector2I[] FindPath(Vector2I startPos, Vector2I targetPos){
+        Vector2I[] waypoints = new Vector2I[0];
+		bool pathSuccess = false;
+		
 		Tile_PF startNode = grid[startPos];
 		Tile_PF targetNode = grid[targetPos];
 
-		Heap openSet = new Heap();
-		HashSet<Tile_PF> closedSet = new HashSet<Tile_PF>();
-		openSet.Add(startNode);
-
-		while (openSet.Count > 0) {
-			Tile_PF currentNode = openSet.RemoveFirst();
-			closedSet.Add(currentNode);
-
-			if (currentNode == targetNode) {
-				RetracePath(startNode,targetNode);
-				return;
-			}
-
-			foreach (Tile_PF neighbour in GetNeighbours(currentNode)) {
-				if (!neighbour.walkable || closedSet.Contains(neighbour)) {
-					continue;
+		if (startNode.walkable && targetNode.walkable) {
+            Heap openSet = new Heap();
+		    HashSet<Tile_PF> closedSet = new HashSet<Tile_PF>();
+		    openSet.Add(startNode);
+			
+			while (openSet.Count > 0) {
+				Tile_PF currentNode = openSet.RemoveFirst();
+				closedSet.Add(currentNode);
+				
+				if (currentNode == targetNode) {
+					pathSuccess = true;
+					break;
 				}
-
-				int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
-				if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
-					neighbour.gCost = newMovementCostToNeighbour;
-					neighbour.hCost = GetDistance(neighbour, targetNode);
-					neighbour.parent = currentNode;
-
-					if (!openSet.Contains(neighbour))
-						openSet.Add(neighbour);
-					else {
-						//openSet.UpdateItem(neighbour);
+				
+				foreach (Tile_PF neighbour in GetNeighbours(currentNode)) {
+					if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+						continue;
+					}
+					
+					int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+					if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+						neighbour.gCost = newMovementCostToNeighbour;
+						neighbour.hCost = GetDistance(neighbour, targetNode);
+						neighbour.parent = currentNode;
+						
+						if (!openSet.Contains(neighbour))
+							openSet.Add(neighbour);
 					}
 				}
 			}
 		}
+		if (pathSuccess) {
+			waypoints = RetracePath(startNode,targetNode);
+		}
+		return waypoints;
 	}
 
-	List<Tile_PF> RetracePath(Tile_PF startNode, Tile_PF endNode) {
+    Vector2I[] RetracePath(Tile_PF startNode, Tile_PF endNode) {
 		List<Tile_PF> path = new List<Tile_PF>();
 		Tile_PF currentNode = endNode;
-
+		
 		while (currentNode != startNode) {
 			path.Add(currentNode);
 			currentNode = currentNode.parent;
 		}
-		path.Reverse();
-
-		return path;
+		Vector2I[] waypoints = SimplifyPath(path);
+		Array.Reverse(waypoints);
+		return waypoints;
+	}
+    Vector2I[] SimplifyPath(List<Tile_PF> path) {
+		List<Vector2I> waypoints = new List<Vector2I>();
+		Vector2I directionOld = Vector2I.Zero;
+		
+		for (int i = 1; i < path.Count; i ++) {
+			Vector2I directionNew = new Vector2I(path[i-1].gridX - path[i].gridX,path[i-1].gridY - path[i].gridY);
+			if (directionNew != directionOld) {
+				waypoints.Add(path[i].tileposition);
+			}
+			directionOld = directionNew;
+		}
+		return waypoints.ToArray();
 	}
 
 	int GetDistance(Tile_PF nodeA, Tile_PF nodeB) {
