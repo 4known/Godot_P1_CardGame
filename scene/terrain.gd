c00d538d8016c584657bc1b2c407c67b67c87796
L13k <Kewenlin12@gmail.com>
2023-12-10 18:12:07 -0500

    ter

--- scene/terrain.gd
+++ scene/terrain.gd
@@ -2,25 +2,21 @@ extends TileMap
 class_name Terrain
 
 @onready var pf = $Pathfinding
+@onready var gen = $TerrainGeneration
 var destinations : PackedVector2Array = []
 
 var size : int = 20
 var noise
 
 func _ready():
+	generateTerrain()
 	for t in get_used_cells(0):
 		pf.AddToGrid(t);
-#	generateTerrain()
 
 func generateTerrain():
 	clear_layer(0)
-	noise = FastNoiseLite.new()
-	noise.seed = randi()
-	noise.frequency *= 20
-	for x in range(-size,size):
-		for y in range(-size,size):
-			var noiseValue = noise.get_noise_2d(x,y)
-			set_cell(0, Vector2i(x,y),1,Vector2i(ceil(noiseValue),0))
+	for p in gen.CreateGrid(13,Vector2i.ZERO):
+		set_cell(0,p,1,Vector2i(1,0))
 
 func getPath(myposition : Vector2i, targetpos : Vector2i, range_ : int, border : bool) -> Array[Vector2i]:
 	var myTilepos = local_to_map(myposition)
--- script/TerrainGeneration/TerrainGeneration.cs
+++ script/TerrainGeneration/TerrainGeneration.cs
@@ -1,8 +1,56 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 [GlobalClass]
 public partial class TerrainGeneration : Node
 {
     
+    public Godot.Collections.Array<Vector2I> CreateGrid(int radius, Vector2I position){
+        var grid = new Godot.Collections.Array<Vector2I>();
+        int radiusSquaredOuter = radius * radius;
+        int radiusSquaredInner = (radius - 5) * (radius - 5);
+
+        FastNoiseLite noise = new FastNoiseLite();
+        noise.Seed = (int)GD.Randi();
+	    noise.Frequency *= 50;
+
+
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int y = -radius; y <= radius; y++)
+            {
+                int positionx = x + position.X;
+                int positiony = y + position.Y;
+
+                float value = noise.GetNoise2D(positionx,positiony);
+                int vector = x * x + y * y;
+                if (radiusSquaredInner < vector && vector < radiusSquaredOuter)
+                {
+                    int dstX = Mathf.Abs(x/radius+1);
+                    int dstY = Mathf.Abs(y/radius+1);
+                    int falloff = Mathf.Max(dstX,dstY);
+                    value += falloff;
+                    if(value >= 1){
+                        grid.Add(new Vector2I(x,y));
+                    }
+                }
+                else if(vector < radiusSquaredInner){
+                    grid.Add(new Vector2I(x,y));
+                }
+            }
+        }
+        return grid;
+    }
+    public void UpdateGrid(List<Vector2I> outer, Godot.Collections.Array<Vector2I> grid){
+        
+    }
 }
+// if (grid[i, j] == 1)
+                // {
+                //     newGrid[i, j] = neighbors == 2 || neighbors == 3 ? 1 : 0;
+                // }
+                // else
+                // {
+                //     newGrid[i, j] = neighbors == 3 ? 1 : 0;
+                // }
