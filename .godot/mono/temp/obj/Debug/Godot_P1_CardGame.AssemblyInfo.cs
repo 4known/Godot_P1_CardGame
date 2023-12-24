beb3c34a619ee8b034a9ec9a983da08248bde6fd
L13k <Kewenlin12@gmail.com>
2023-12-24 15:37:43 -0500

    state

--- .godot/mono/temp/obj/Debug/Godot_P1_CardGame.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Godot_P1_CardGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Godot_P1_CardGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b85f8b0eefbdfefa1b3241a2ca34b4d731d774ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91879487ea2869b1ec8163ced36e4c21b174413e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Godot_P1_CardGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Godot_P1_CardGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- components/GameState.gd
+++ components/GameState.gd
@@ -10,7 +10,6 @@ const card = preload("res://Scene/card.tscn")
 
 var enemynum : int = 1
 var playernum : int = 10
-var spawnpos : Array[Vector2i] = []
 
 enum states{GameLoad, PlayerTurn, EntityTurn}
 var currentState : states = states.GameLoad
@@ -32,8 +31,8 @@ func updateState():
 	print("Turn: " + str(turnNum))
 	match currentState:
 		states.GameLoad:
-			loadGame()
 			currentState = states.PlayerTurn
+			loadGame()
 		states.PlayerTurn:
 			print("PlayerTurn")
 			emit_signal("newTurn", currentState)
@@ -51,8 +50,8 @@ func loadGame():
 	#Terrain Generation
 	ter.initGeneration()
 	spawnEnemy()
-	#spawnPlayer()
-	#updateState()
+	spawnPlayer()
+	updateState()
 
 func playerTurn():
 	for p in players.get_children():
@@ -64,17 +63,15 @@ func entityTurn():
 
 func spawnPlayer():
 	for i in range(playernum):
-		var pos : Vector2i
-		while spawnpos.has(pos) or ter.get_cell_source_id(0,ter.local_to_map(pos)) == -1:
-			pos = ter.map_to_local(Vector2i(randi_range(-5,5),randi_range(-5,5)))
-		spawnpos.append(pos)
-		ter.pf.SetNodeOccupied(ter.local_to_map(pos),true)
-		var newCard = initilizeCard(pos, Card.types.player)
+		var pos : Vector2i = ter.currentRoom.grid.pick_random()
+		while ter.pf.IsOccupied(pos):
+			pos = ter.currentRoom.grid.pick_random()
+		var newCard = initilizeCard(ter.map_to_local(pos), Card.types.player)
 		newCard.name = "Player" + str(i)
 		players.add_child(newCard)
 
 func spawnEnemy():
-	var i = 0
+	var i = 1
 	for pos in ter.currentRoom.enemiesPosition:
 		var newCard = initilizeCard(ter.map_to_local(pos), Card.types.enemy)
 		newCard.name = "Enemy" + str(i)
